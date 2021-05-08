a8851ae98796bd2d7cf91650e18b42d401898c2e
byen2 <benoit.yen@sjsu.edu>
2021-05-08 13:44:54 -0700

    add fireball ammo

--- CurrentProject/AbstractProjectile.java
+++ CurrentProject/AbstractProjectile.java
@@ -7,7 +7,11 @@ public class AbstractProjectile extends Actor implements IProjectileShooter{
 	public AbstractProjectile()
 	{
 	    removed = false;
+			//default ammo
+			//for now set image here, between fireball/ammo
+			setImage(new GreenfootImage("ammo.png"));
 	}
+
 	public void act()
 	{
 	    if(removed){return;}
---
+++ CurrentProject/FireBall.java
@@ -0,0 +1,12 @@
+import greenfoot.*;
+
+public class FireBall extends AbstractProjectile{
+
+  boolean removed;
+
+  public FireBall(){
+    removed = false;
+    setImage(new GreenfootImage("fireball.png"));
+  }
+
+}
