4aabdaf8bc863495f1833cee34a5dc33980ae826
byen2 <benoit.yen@sjsu.edu>
2021-05-12 21:03:56 -0700

    add wood sound effect

--- CurrentProject/WoodPlatform.java
+++ CurrentProject/WoodPlatform.java
@@ -1,11 +1,20 @@
 import greenfoot.*;
 
 public class WoodPlatform extends AbstractPlatform {
+  //wood break sound effect taken from zapsplat.com
+  private GreenfootSound soundSound = new GreenfootSound("wood_break.wav");
+  private boolean hasPlayed = false;
+
   public WoodPlatform(float bounceForce) {
       super(bounceForce);
   }
 
   public void onBounce() {
+      //play only once
+      if(hasPlayed == false){
+          soundSound.play();
+          hasPlayed = true;
+      }
       setImage(new GreenfootImage("broken_platform.png"));
       goThrough = true;
   }
