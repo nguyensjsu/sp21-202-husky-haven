6df544d93c08fd8eaaf18452b1f9e546428aecd5
Nate Diamond <nathan.diamond@sjsu.edu>
2021-05-15 16:01:30 -0700

    Fixed null pointer error for shooting when dead

--- CurrentProject/DefaultShootingStrategy.java
+++ CurrentProject/DefaultShootingStrategy.java
@@ -3,15 +3,19 @@ import greenfoot.*;
 
 public class DefaultShootingStrategy extends AbstractShootingStrategy {
 
-    //private GreenfootSound pellet_sfx = new GreenfootSound("pellet_shot.mp3");
+    private GreenfootSound pellet_sfx = new GreenfootSound("pellet_shot.mp3");
 
     public DefaultShootingStrategy(World world) {
         super(world, 100);
     }
 
     public void handleShooting(Actor shooter, boolean fromPlayer) {
-        //pellet_sfx.setVolume(25);
-        //pellet_sfx.play();
+        pellet_sfx.setVolume(25);
+        pellet_sfx.play();
+        
+        if (shooter == null)
+            return;
+        
         Pellet p = new Pellet(fromPlayer, 270);
         world.addObject(p, shooter.getX(), shooter.getY() - shooter.getImage().getHeight());
     }
--- CurrentProject/FireShootingStrategy.java
+++ CurrentProject/FireShootingStrategy.java
@@ -3,15 +3,19 @@ import greenfoot.*;
 
 public class FireShootingStrategy extends AbstractShootingStrategy {
 
-    //private GreenfootSound fireball_sfx = new GreenfootSound("fireball_shot.mp3");
+    private GreenfootSound fireball_sfx = new GreenfootSound("fireball_shot.mp3");
 
     public FireShootingStrategy(World world) {
         super(world, 500);
     }
 
     public void handleShooting(Actor shooter, boolean fromPlayer) {
-        //fireball_sfx.setVolume(25);
-        //fireball_sfx.play();
+        fireball_sfx.setVolume(25);
+        fireball_sfx.play();
+        
+        if (shooter == null)
+            return;
+            
         for (int i = 0; i < 3; i++) {
             Fireball fb = new Fireball(fromPlayer, 250 + 20 * i);
             world.addObject(fb, shooter.getX() - 1 + i, shooter.getY() - shooter.getImage().getHeight());
